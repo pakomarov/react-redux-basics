1d2ed72eda369f1f1e9f1024317310210a1dd129
Pavel Komarov <komarov.pa.it@gmail.com>
2020-11-15 22:40:06 +0400

    Dev utilities: logger and redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 import cake from './cake/reducer';
 import icecream from './icecream/reducer';
 
@@ -7,6 +9,8 @@ const rootReducer = combineReducers({
   icecream,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(logger)
+));
 
 export default store;
