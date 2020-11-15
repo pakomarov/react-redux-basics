914579c04f5843b2c268eeb09ea3c00a6e99c611
Pavel Komarov <komarov.pa.it@gmail.com>
2020-11-15 20:42:04 +0400

    React-redux with hooks

---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { buyCake } from '../redux';
+
+function HooksCakeContainer() {
+  const cakeCount = useSelector(state => state.cakeCount);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Num of cakes - {cakeCount}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy cake</button>
+    </div>
+  );
+}
+
+export default HooksCakeContainer;
